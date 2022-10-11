b96124efca29fd0655aa15bde9fc1166534be394
César Joel González Oliva <113487509+ChechaWebDesigner@users.noreply.github.com>
2022-10-11 09:40:27 -0600

    Add files via upload

---
+++ Data/data-projects.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    name: "Calculator APP",
+    link: "https://chechawebdesigner.github.io/Calculator/",
+    description: "This calculator let us  doing the simple operation",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    img: "./images/calculator-web-site.png",
+  },
+  {
+    name: "FAQ Card",
+    link: "https://chechawebdesigner.github.io/FAQ-Card/",
+    description: "It's a simple card with some animations and events",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    img: "./images/FAQ-Card.png",
+  },
+  {
+    name: "Task List",
+    link: "https://chechawebdesigner.github.io/Task-List/",
+    description: "It is a list of tasks that notifies us when the task expires",
+    technologies: ["HTML", "Tailwind CSS", "JavaScript"],
+    img: "./images/task-list.png",
+  },
+  {
+    name: "Interactive Pricing Component",
+    link: "https://chechawebdesigner.github.io/Interactive-Pricing-Component/",
+    description:
+      "It is an interactive pricing where you can change the views and with it the price",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    img: "./images/interactive-component.png",
+  },
+  {
+    name: "Subway Page",
+    link: "https://subwayproject.000webhostapp.com/app/Web5BACOB07/index.html",
+    description: "It is an academic project about the company Subway",
+    technologies: ["HTML", "Bootstrap", "JavaScript", "PHP"],
+    img: "./images/subway.png",
+  },
+  // {
+  //   name: "Yuuchi Puzle",
+  //   link: "https://chechawebdesigner.github.io/Yuchi-Puzzle/",
+  //   description: "Es un pequeño rompecabezas sobre Katagiri Yuuichi",
+  //   img: "./images/interactive-component.png",
+  // },
+];
